bd89c767079f8a0524914e7328bc2c0032604dc4
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-08-18 10:13:33 +0100

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,36 +1,3 @@
 <?php
 	
-	if (!function_exists('view')) {
-	    /**
-	     * Helper function to build views.
-	     *
-	     * @param string $view      The view relative path, name.
-	     * @param array  $data      Passed data.
-	     * @param array  $mergeData
-	     *
-	     * @return string
-	     */
-	    function view($view = null, array $data = [], array $mergeData = [])
-	    {
-	        $factory = app('view');
-	        if (func_num_args() === 0) {
-	            return $factory;
-	        }
-	        return $factory->make($view, $data, $mergeData)->render();
-	    }
-	}
 	
-	if( ! function_exists( 'shortcode_path' ) ) {
-		
-		function shortcode_path( $file = '' ) {
-			
-		    if( function_exists('themosis_path') ) {
-			    $path = themosis_path('theme.resources') . DS . 'shortcodes';
-		    } else {
-			    $path = get_stylesheet_directory() . DS . 'resources' . DS . 'shortcodes';
-		    }
-		    return $path . ltrim( ( $file ? DS . $file : '' ), DS );
-			
-		}
-		
-	}
